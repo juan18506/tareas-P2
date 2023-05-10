4154f4f38a81c0efe97a2f90b57aeff6fdb74ae9
Juan18506 <juan185067@gmail.com>
2023-05-10 18:17:59 -0300

    Hasta test pila-combinado BIEN

--- tarea3/src/pilaPersona.cpp
+++ tarea3/src/pilaPersona.cpp
@@ -2,29 +2,33 @@
 #include "../include/personasLDE.h"
 
 struct rep_pilaPersona {
-
+  TPersonasLDE pila;
 };
 
 TPilaPersona crearTPilaPersona() {
-  return NULL;
+  TPilaPersona nuevaPila = new rep_pilaPersona;
+  nuevaPila->pila = crearTPersonasLDE();
+  return nuevaPila;
 }
 
 void liberarTPilaPersona(TPilaPersona &p) {
-
+  liberarTPersonasLDE(p->pila);
+  delete p;
+  p = NULL;
 }
 
 nat cantidadEnTPilaPersona(TPilaPersona p) { 
-  return 0;
+  return cantidadTPersonasLDE(p->pila);
 }
 
 void apilarEnTPilaPersona(TPilaPersona &p, TPersona persona) {
-
+  insertarInicioDeTPersonasLDE(p->pila, copiarTPersona(persona));
 }
 
 TPersona cimaDeTPilaPersona(TPilaPersona p) { 
-  return NULL;
+  return obtenerInicioDeTPersonasLDE(p->pila);
 }
 
 void desapilarDeTPilaPersona(TPilaPersona &p) {
-
+  eliminarInicioTPersonasLDE(p->pila);
 }
