8bd458d9692f910c43f8400ff376037e3be95a20
Juan18506 <juan185067@gmail.com>
2023-05-14 22:38:22 -0300

    Hasta aplicaciones1-menores BIEN

--- tarea3/src/aplicaciones.cpp
+++ tarea3/src/aplicaciones.cpp
@@ -1,9 +1,33 @@
 #include "../include/aplicaciones.h"
 
 TPilaPersona menoresQueElResto(TPersonasLDE lista) {
-    return NULL;
+    TPilaPersona pilaLista = crearTPilaPersona();
+    TPilaPersona pilaResultado = crearTPilaPersona();
+
+    if (cantidadTPersonasLDE(lista) == 0) {
+        return pilaResultado;
+    }
+
+    apilarEnTPilaPersona(pilaLista, obtenerFinalDeTPersonasLDE(lista));
+    eliminarFinalTPersonasLDE(lista);
+
+    while (cantidadTPersonasLDE(lista) > 0) {
+        if (edadTPersona(cimaDeTPilaPersona(pilaLista)) > edadTPersona(obtenerFinalDeTPersonasLDE(lista))) {
+            apilarEnTPilaPersona(pilaLista, obtenerFinalDeTPersonasLDE(lista));
+        }
+
+        eliminarFinalTPersonasLDE(lista);
+    }
+
+    while (cantidadEnTPilaPersona(pilaLista) > 0) {
+        apilarEnTPilaPersona(pilaResultado, cimaDeTPilaPersona(pilaLista));
+        desapilarDeTPilaPersona(pilaLista);
+    }
+
+    liberarTPilaPersona(pilaLista);
+    return pilaResultado;
 }
 
-bool sumaPares(nat k, TConjuntoIds c){
+bool sumaPares(nat k, TConjuntoIds c) {
     return false;
 }
