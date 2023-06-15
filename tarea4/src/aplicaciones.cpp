b2a937e57e955039afdc900bcbf2fa4d15356f2c
Juan18506 <juan185067@gmail.com>
2023-06-15 19:55:37 -0300

    Perfesto :)

--- tarea4/src/aplicaciones.cpp
+++ tarea4/src/aplicaciones.cpp
@@ -1,5 +1,26 @@
 #include "../include/aplicaciones.h"
+#include "../include/colaDePrioridadPersona.h"
 
-void listarEnOrden(TTablaPersonas t, char** nombres, nat n){
+void listarEnOrden(TTablaPersonas t, char **nombres, nat n) {
+    TColaDePrioridadPersona cp = crearCP(MAX_ID);
 
+    for (nat i = 0; i < n; i++) {
+        if (!perteneceATTablaPersonas(t, nombres[i])) {
+            continue;
+        }
+
+        TPersona persona = obtenerPersonaDeTTablaPersonas(t, nombres[i]);
+        nat id = idTPersona(persona);
+        if (id <= MAX_ID && !estaEnCP(id, cp)) {
+            insertarEnCP(copiarTPersona(persona), cp);
+        }
+    }
+
+    while (!estaVaciaCP(cp)) {
+        TPersona persona = prioritaria(cp);
+        imprimirTPersona(persona);
+        eliminarPrioritaria(cp);
+    }
+
+    liberarCP(cp);
 }
